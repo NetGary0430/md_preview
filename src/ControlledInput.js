e4595cbb1636aae4b824042f8a27800de4903c51
Gary Netherton <garynetherton@gmail.com>
2020-08-09 19:31:37 -0700

    Fix spacing issues and use of markedjs Used CodeSandBox to import MarkedJS and change the implementation.

--- src/ControlledInput.js
+++ src/ControlledInput.js
@@ -1,39 +1,41 @@
-import React from 'react';
-
+import React from "react";
+let marked = require("marked");
 
 class ControlledInput extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        input: ''
-      };
-      this.handleChange = this.handleChange.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleChange(event) {
-      this.setState({
-        input: event.target.value
-      });
-    }
-    render() {
-      return (
-        <div>
-         <textarea id = "editor"
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  render() {
+    return (
+      <div>
+        <textarea
+          id="editor"
           value={this.state.textAreaValue}
           onChange={this.handleChange}
           rows={5}
           cols={35}
         />
 
-          <h4>Controlled Input:</h4>
-  
-          <p id="preview">{this.state.input}</p>
-          {<script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>}
-          {<script> document.getElementById('content').innerHTML =
-            marked('# Marked in browser\n\nRendered by **marked**.');</script>}
-        </div>
-      );
-    }
-  };
+        <h4>Markdown Preview:</h4>
 
-  export default ControlledInput;
+        <hr />
+        <p
+          id="preview"
+          dangerouslySetInnerHTML={{ __html: marked(this.state.input) }}
+        ></p>
+      </div>
+    );
+  }
+}
+
+export default ControlledInput;
